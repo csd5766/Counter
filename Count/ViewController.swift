1606a020a25154bc7e82464324835064c286e3fa
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-03-20 16:10:01 +0900

    버튼클릭시 레이블에 출력하기

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ViewCount: UILabel!
+        var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnt(_ sender: Any) {
+        if (count < 10) {
+            count += 1
+            ViewCount.text = String(count)
+            
+        }
+        else if(count == 10) {
+            count = 0
+        }
+        
+        
+        
+    }
+    
 }
 
